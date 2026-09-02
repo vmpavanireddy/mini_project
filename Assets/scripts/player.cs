78dd375064429de033c9bf006da2feba24d64e87
vmpavanireddy <vmpavanireddy@gmail.com>
2026-09-03 01:19:55 +0530

    animate idle

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float speed;
+    private float input;
     Rigidbody2D rb;
+    Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        anim=GetComponent<Animator>();
         rb=GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if(input !=0)
+        {
+            anim.SetBool("isrunning",true);
+        }
+        else
+        {
+            anim.SetBool("isrunning",false);
+        }
+        if(input>0)
+        {
+            transform.eulerAngles=new Vector3(0,0,0);
+        }
+        else if(input<0)
+        {
+            transform.eulerAngles=new Vector3(0,180,0);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-        float input=Input.GetAxisRaw("Horizontal");
+        input=Input.GetAxisRaw("Horizontal");
             Debug.Log(input);
         rb.linearVelocity=new Vector2(input*speed,rb.linearVelocity.y);
     }

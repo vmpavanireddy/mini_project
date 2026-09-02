3e391e4438277c6d85c09a090538654803decd65
vmpavanireddy <vmpavanireddy@gmail.com>
2026-09-03 01:42:49 +0530

    speed for enemy

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    public float minspeed;
+    public float maxspeed;
+    float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        speed=Random.Range(minspeed,maxspeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.down*speed*Time.deltaTime);
     }
 }

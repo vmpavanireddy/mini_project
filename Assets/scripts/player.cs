9978ad58dd40fb11927ea14dca5fe7bc07e94ce3
vmpavanireddy <vmpavanireddy@gmail.com>
2026-09-03 22:08:38 +0530

    dash time and final game input

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -13,31 +13,59 @@ public class player : MonoBehaviour
     Animator anim;
     AudioSource audiosource;
     public int health;
+
+
+    public float startdashtime;
+    private float dashtime;
+    public float extraspeed;
+    private bool isdashing;
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        audiosource=GetComponent<AudioSource>();
-        anim=GetComponent<Animator>();
-        rb=GetComponent<Rigidbody2D>();
-        healthtext.text=health.ToString();
+        audiosource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        healthtext.text = health.ToString();
     }
     private void Update()
     {
-        if(input !=0)
+        if (input != 0)
         {
-            anim.SetBool("isrunning",true);
+            anim.SetBool("isrunning", true);
         }
         else
         {
-            anim.SetBool("isrunning",false);
+            anim.SetBool("isrunning", false);
         }
-        if(input>0)
+        if (input > 0)
         {
-            transform.eulerAngles=new Vector3(0,0,0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        else if(input<0)
+        else if (input < 0)
         {
-            transform.eulerAngles=new Vector3(0,180,0);
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isdashing)
+        {
+            isdashing = true;
+            dashtime = startdashtime;
+            speed += extraspeed;
+        }
+
+        if (isdashing)
+        {
+            dashtime -= Time.deltaTime;
+
+            if (dashtime <= 0)
+            {
+                isdashing = false;
+                speed -= extraspeed;
+            }
         }
     }
 
@@ -45,15 +73,15 @@ public class player : MonoBehaviour
     void FixedUpdate()
     {
 
-        input=Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity=new Vector2(input*speed,rb.linearVelocity.y);
+        input = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(input * speed, rb.linearVelocity.y);
     }
     public void takeDamage(int damageamount)
     {
+        health -= damageamount;
         audiosource.Play();
-        health-=damageamount;
-        healthtext.text=health.ToString();
-        if(health<=0)
+        healthtext.text = health.ToString();
+        if (health <= 0)
         {
             losepanel.SetActive(true);
             Destroy(gameObject);
